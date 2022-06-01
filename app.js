650e020c77a133d8623ce0ffa554293cdf2e029f
OtnielPerez <otnielperez05@gmail.com>
2022-06-01 09:12:28 -0400

    The computer option and the game option was added

--- app.js
+++ app.js
@@ -27,6 +27,34 @@ const game = () =>{
                 this.style.animation = "";
               });
         });
-       
-};
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `./img/${this.textContent}.png`;
+          computerHand.src = `./img/${computerChoice}.png`;
+        }, 2000);
+        //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+ 
+ };
+
+ const updateScore = () =>{
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;  
+ };
 }
